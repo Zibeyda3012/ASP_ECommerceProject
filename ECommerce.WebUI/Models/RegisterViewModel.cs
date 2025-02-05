d7b8d997de087d3c2371ced66d9b32f7736a174a
Musayeva Zibeyda Rovshan <Musay_zv97@itstep.edu.az>
2025-02-05 12:23:15 +0400

    identity library

---
+++ ECommerce.WebUI/Models/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.WebUI.Models;
+
+public class RegisterViewModel
+{
+    public required string Username { get; set; }
+
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+}
--- ECommerce.WebUI/Program.cs
+++ ECommerce.WebUI/Program.cs
@@ -4,7 +4,9 @@ using ECommerce.Application.Concrete;
 using ECommerce.DataAccess.Abstact;
 using ECommerce.DataAccess.Concerete.EFEntityFramework;
 using ECommerce.DataAccess.Context;
+using ECommerce.WebUI.Entities;
 using ECommerce.WebUI.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,10 +26,15 @@ builder.Services.AddScoped<ICartService,CartService>();
 #region Database registration
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<NorthWindDbContext>(opt =>
-{
-    opt.UseSqlServer(conn);
-});
+//builder.Services.AddDbContext<NorthWindDbContext>(opt =>
+//{
+//    opt.UseSqlServer(conn);
+//});
+
+builder.Services.AddDbContext<CustomIdentityDbContext>(opt => opt.UseSqlServer(conn));
+
+builder.Services.AddIdentity<CustomIdentityUser, CustomIdentityRole>().
+    AddEntityFrameworkStores<CustomIdentityDbContext>().AddDefaultTokenProviders();
 #endregion
 
 
@@ -49,6 +56,6 @@ app.UseAuthorization();
 app.UseSession();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Register}/{id?}");
 
 app.Run();
